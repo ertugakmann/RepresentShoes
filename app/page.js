0275889faae371e308a86173667e2cebe6eab26b
Ertuğ Akman <ertug3470@gmail.com>
2024-12-01 18:22:21 +0000

    Implement Firebase integration and add getCities function

--- app/page.js
+++ app/page.js
@@ -1,6 +1,29 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect } from "react";
+import { db } from "../lib/firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+import { getCities } from "../lib/firebase";
 
 export default function Home() {
+    // DATABASE TESTING
+
+    // useEffect(() => {
+    //     const getUsers = async () => {
+    //         const usersCollection = collection(db, "users");
+    //         const userSnapshot = await getDocs(usersCollection);
+
+    //         console.log(userSnapshot.docs.map((doc) => doc.data()));
+    //     };
+
+    //     getUsers();
+    // }, []);
+
+    useEffect(() => {
+        getCities(db);
+    }, []);
+
     return (
         <div>
             <h1 className="text-green-300">Hello World!</h1>
