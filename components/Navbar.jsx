a340ea40389835b8ddc9e449db1e5ac728371601
Ertug Akman <134059022+ertugakmann@users.noreply.github.com>
2024-12-05 23:32:52 +0000

    Enhance Navbar with search functionality and update image domains in config.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -110,9 +110,9 @@ const Navbar = () => {
               </button>
             </li>
             <li>
-              <button onClick={() => setOverlayContent('cart')}>
+              <Link href={"/login"}>
                 <SlBasket size={18} />
-              </button>
+              </Link>
             </li>
           </ul>
           <ul className="flex lg:hidden items-center gap-x-5">
@@ -177,18 +177,182 @@ const Navbar = () => {
     </div>
   </div>
 )}
+          {overlayContent === 'search' &&(
+            <div className='flex flex-col h-full p-4'>
+              <h3 className='text-2xl font-semibold'>Search</h3>
+              <input type='text' className='border-2 border-gray-300 mt-4 p-2' placeholder='Search for products' />
+              
+              <div className='flex flex-col'>
+                <h3 className='text-sm font-semibold text-gray-500 mt-5'>
+                  Top Searches
+                </h3>
+                <ul>
+                  <li><Link href={"/login"}> <span className='text-xs text-gray-600 font-semibold'>Streetwear T-Shirts & Graphic Tees</span></Link></li>
+                  <li><Link href={"/login"}> <span className='text-xs text-gray-600 font-semibold'>New Arrivals</span></Link></li>
+                  <li><Link href={"/login"}> <span className='text-xs text-gray-600 font-semibold'>Streetwear Hoodies</span></Link></li>
+                  <li><Link href={"/login"}> <span className='text-xs text-gray-600 font-semibold'>247</span></Link></li>
+                </ul>
 
-          {overlayContent === 'search' && <p className="text-xl">Search Content</p>}
+        {/* Searched Products */}
+<div className='w-full mt-6 overflow-x-auto'>
+  <div className='grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4'>
+    <Link href={"/"}>
+      <div className='flex flex-col'>
+        <Image
+          width={200}
+          height={200}
+          alt='product-image'
+          className='w-full h-auto object-cover'
+          src="https://sfycdn.speedsize.com/fbaf6506-81e1-43a2-bcc1-80e18c7b0146/https://cdn.shopify.com/s/files/1/0181/2235/products/SCALED__0041_BLACKOWNERSCLUBHOODIE_0001_REPRESENTOWNERSCLUBBACKPRINT.jpg?v=1685532891&width=2000"
+        />
+        <div className='mt-2 flex flex-col'>
+          <span className='text-sm font-semibold'>Represent Owners Club Hoodie</span>
+          <span className='text-xs text-gray-500 font-semibold'>Black 25 Colours</span>
+          <span>110£</span>
+        </div>
+      </div>
+    </Link>
+    <Link href={"/"}>
+      <div className='flex flex-col'>
+        <Image
+          width={200}
+          height={200}
+          alt='product-image'
+          className='w-full h-auto object-cover'
+          src="https://sfycdn.speedsize.com/fbaf6506-81e1-43a2-bcc1-80e18c7b0146/https://cdn.shopify.com/s/files/1/0181/2235/products/SCALED__0041_BLACKOWNERSCLUBHOODIE_0001_REPRESENTOWNERSCLUBBACKPRINT.jpg?v=1685532891&width=2000"
+        />
+        <div className='mt-2 flex flex-col'>
+          <span className='text-sm font-semibold'>Represent Owners Club Hoodie</span>
+          <span className='text-xs text-gray-500 font-semibold'>Black 25 Colours</span>
+          <span>110£</span>
+        </div>
+      </div>
+    </Link>
+    <Link href={"/"}>
+      <div className='flex flex-col'>
+        <Image
+          width={200}
+          height={200}
+          alt='product-image'
+          className='w-full h-auto object-cover'
+          src="https://sfycdn.speedsize.com/fbaf6506-81e1-43a2-bcc1-80e18c7b0146/https://cdn.shopify.com/s/files/1/0181/2235/products/SCALED__0041_BLACKOWNERSCLUBHOODIE_0001_REPRESENTOWNERSCLUBBACKPRINT.jpg?v=1685532891&width=2000"
+        />
+        <div className='mt-2 flex flex-col'>
+          <span className='text-sm font-semibold'>Represent Owners Club Hoodie</span>
+          <span className='text-xs text-gray-500 font-semibold'>Black 25 Colours</span>
+          <span>110£</span>
+        </div>
+      </div>
+    </Link>
+    <Link href={"/"}>
+      <div className='flex flex-col'>
+        <Image
+          width={200}
+          height={200}
+          alt='product-image'
+          className='w-full h-auto object-cover'
+          src="https://sfycdn.speedsize.com/fbaf6506-81e1-43a2-bcc1-80e18c7b0146/https://cdn.shopify.com/s/files/1/0181/2235/products/SCALED__0041_BLACKOWNERSCLUBHOODIE_0001_REPRESENTOWNERSCLUBBACKPRINT.jpg?v=1685532891&width=2000"
+        />
+        <div className='mt-2 flex flex-col'>
+          <span className='text-sm font-semibold'>Represent Owners Club Hoodie</span>
+          <span className='text-xs text-gray-500 font-semibold'>Black 25 Colours</span>
+          <span>110£</span>
+        </div>
+      </div>
+    </Link>
+  </div>
+</div>
+
+              </div>
+            </div>
+          )}
           {overlayContent === 'notifications' && (
             <p className="text-xl">Notifications Content</p>
           )}
           {overlayContent === 'wishlist' && (
             <p className="text-xl">Wishlist Content</p>
           )}
-          {overlayContent === 'profile' && (
-            <p className="text-xl">Profile Content</p>
+          {overlayContent === 'cart' && (
+            <>
+            <div className='w-full mt-6 overflow-x-auto'>
+                <p className='text-xl font-semibold'>Cart Content</p>
+            <div className='grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4 mt-12'>
+              <Link href={"/"}>
+                <div className='flex flex-col'>
+                  <Image
+                    width={200}
+                    height={200}
+                    alt='product-image'
+                    className='w-full h-auto object-cover'
+                    src="https://sfycdn.speedsize.com/fbaf6506-81e1-43a2-bcc1-80e18c7b0146/https://cdn.shopify.com/s/files/1/0181/2235/products/SCALED__0041_BLACKOWNERSCLUBHOODIE_0001_REPRESENTOWNERSCLUBBACKPRINT.jpg?v=1685532891&width=2000"
+                  />
+                  <div className='mt-2 flex flex-col'>
+                    <span className='text-sm font-semibold'>Represent Owners Club Hoodie</span>
+                    <span className='text-xs text-gray-500 font-semibold'>Black 25 Colours</span>
+                    <span>110£</span>
+                  </div>
+                </div>
+              </Link>
+              <Link href={"/"}>
+                <div className='flex flex-col'>
+                  <Image
+                    width={200}
+                    height={200}
+                    alt='product-image'
+                    className='w-full h-auto object-cover'
+                    src="https://sfycdn.speedsize.com/fbaf6506-81e1-43a2-bcc1-80e18c7b0146/https://cdn.shopify.com/s/files/1/0181/2235/products/SCALED__0041_BLACKOWNERSCLUBHOODIE_0001_REPRESENTOWNERSCLUBBACKPRINT.jpg?v=1685532891&width=2000"
+                  />
+                  <div className='mt-2 flex flex-col'>
+                    <span className='text-sm font-semibold'>Represent Owners Club Hoodie</span>
+                    <span className='text-xs text-gray-500 font-semibold'>Black 25 Colours</span>
+                    <span>110£</span>
+                  </div>
+                </div>
+              </Link>
+              <Link href={"/"}>
+                <div className='flex flex-col'>
+                  <Image
+                    width={200}
+                    height={200}
+                    alt='product-image'
+                    className='w-full h-auto object-cover'
+                    src="https://sfycdn.speedsize.com/fbaf6506-81e1-43a2-bcc1-80e18c7b0146/https://cdn.shopify.com/s/files/1/0181/2235/products/SCALED__0041_BLACKOWNERSCLUBHOODIE_0001_REPRESENTOWNERSCLUBBACKPRINT.jpg?v=1685532891&width=2000"
+                  />
+                  <div className='mt-2 flex flex-col'>
+                    <span className='text-sm font-semibold'>Represent Owners Club Hoodie</span>
+                    <span className='text-xs text-gray-500 font-semibold'>Black 25 Colours</span>
+                    <span>110£</span>
+                  </div>
+                </div>
+              </Link>
+              <Link href={"/"}>
+                <div className='flex flex-col'>
+                  <Image
+                    width={200}
+                    height={200}
+                    alt='product-image'
+                    className='w-full h-auto object-cover'
+                    src="https://sfycdn.speedsize.com/fbaf6506-81e1-43a2-bcc1-80e18c7b0146/https://cdn.shopify.com/s/files/1/0181/2235/products/SCALED__0041_BLACKOWNERSCLUBHOODIE_0001_REPRESENTOWNERSCLUBBACKPRINT.jpg?v=1685532891&width=2000"
+                  />
+                  <div className='mt-2 flex flex-col'>
+                    <span className='text-sm font-semibold'>Represent Owners Club Hoodie</span>
+                    <span className='text-xs text-gray-500 font-semibold'>Black 25 Colours</span>
+                    <span>110£</span>
+                  </div>
+                </div>
+              </Link>
+            </div>
+
+
+            {/* If it is empty */}
+{/* 
+            <div className='w-full h-full flex items-center justify-center'>
+              <p className='font-semibold text-xl'>Your cart is empty...</p>
+            </div> */}
+            
+          </div>
+          </>
           )}
-          {overlayContent === 'cart' && <p className="text-xl">Cart Content</p>}
         </div>
       )}
     </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      domains: ['sfycdn.speedsize.com', 'cdn.shopify.com'], // Buraya kullanacağınız domainleri ekleyin
+    },
+  };
 
 export default nextConfig;
